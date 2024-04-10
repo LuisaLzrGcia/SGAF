a358cd431f7531aa413e1122e2477109967a4d4a
LuisaLzrGcia <isic19.mlazarog@itesco.edu.mx>
2024-04-10 16:35:29 -0600

    first commit

---
+++ app/Componets/Navbar.php
@@ -0,0 +1,43 @@
+<link rel="stylesheet" href="../../public/CSS/style.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary px-5">
+    <div class="d-flex">
+        <div class="">
+            <img class="logo-image" src="" alt="" srcset="../../public/images/logo.png">
+        </div>
+        <div class="d-flex">
+            <div class="mx-2 d-flex justify-content-center align-items-center">
+                <a class="btn btn-info d-flex justify-content-center align-items-center" 
+                href="../Main/Main.php">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-house" viewBox="0 0 16 16">
+                            <path
+                                d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+                        </svg>
+                    </div>
+                    <div class="ps-1">
+                        Inicio
+                    </div>
+                </a>
+            </div>
+            <div class="mx-2 d-flex justify-content-center align-items-center">
+                <a class="btn btn-info" href="http://">Funciones</a>
+            </div>
+            <div class="mx-2 d-flex justify-content-center align-items-center">
+                <a class="btn btn-info" href="http://">Acerca de</a>
+            </div>
+            <div class="mx-2 d-flex justify-content-center align-items-center">
+                <a class="btn btn-info" href="http://">Ayuda</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
---
+++ app/Registrar/ResgistrarUsuario.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registra usuarios</title>
+    <link rel="stylesheet" href="../../public/CSS/style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+</head>
+
+<body class="bg-general">
+    <?php
+    include_once "../Componets/Navbar.php";
+    ?>
+    <div class="mx-5">
+        <h1 class="text-white">
+            Registrar usuario
+        </h1>
+
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    </div>
+</body>
+
+</html>
