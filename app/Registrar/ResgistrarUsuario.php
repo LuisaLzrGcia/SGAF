8a4620effe87f609457384cc1ec52bb3d4bb973d
Brian <isic19.esandovalf@itesco.edu.mx>
2024-04-11 13:49:33 -0600

    RespaldoForm

--- app/Registrar/ResgistrarUsuario.php
+++ app/Registrar/ResgistrarUsuario.php
@@ -7,38 +7,77 @@
     <title>Registra usuarios</title>
     <link rel="stylesheet" href="../../public/CSS/style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+    <style>
+    /* Agrega un margen inferior al contenedor de navegación */
+    nav {
+        margin-bottom: 20px;
+        /* Ajusta el valor según el espacio deseado */
+    }
+
+    /* Estilos para la separación entre cajas de texto */
+    .form-group:not(:last-child) {
+            margin-bottom: 10px; /* Agrega un margen inferior de 20px a todos los .form-group excepto el último */
+        }
+
+    </style>
 </head>
 
 <body class="bg-general">
     <?php
     include_once "../Componets/Navbar.php";
     ?>
-    <div class="container contact-form border m-x:20px ">
-    <div class="container-fluid w-50 ">
-            <div class="contact-image border">
-                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+    <div class="container contact-form  ; ">
+        <div class="container-fluid w-50 ">
+            <div class="contact-image  text-center">
+                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact" />
             </div>
             <form method="post">
-                <h3>Drop Us a Message</h3>
-               <div class="row">
+                <h3 class="text-center">REGISTRO DE USUARIO</h3>
+                <div class="row justify-content-center align-items-center border">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                            <input type="text" name="id" class="form-control" placeholder="ID DE USUARIO *"
+                                value="" />
                         </div>
                         <div class="form-group">
-                            <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                            <input type="text" name="name" class="form-control" placeholder="NOMBRE*" value="" />
                         </div>
                         <div class="form-group">
-                            <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
+                            <input type="text" name="apellidoP" class="form-control" placeholder="APELLIDO PATERNO *"
+                                value="" />
                         </div>
                         <div class="form-group">
-                            <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+                            <input type="text" name="apellidoM" class="form-control" placeholder="APELLIDO MATERNO *"
+                                value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="empresa" class="form-control" placeholder="NOMBRE DE LA EMPRESA *"
+                                value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="direccion" class="form-control" placeholder="DIRECCION *"
+                                value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="rfc" class="form-control" placeholder="RFC *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="phone" class="form-control" placeholder="NUMERO DE TELEFONO *"
+                                value="" />
+                        </div>
+                        <div class="form-group">
+                            <h6 for="txtDate">FECHA DE REGISTRO:</h6>
+                            <input type="date" name="fecha" class="form-control" >
+                        </div>
+
+                        <div class="form-group  text-center">
+                        <button type="button" class="btn btn-primary float-end" href="#">REGISTRAR</button>
                         </div>
                     </div>
-                    
+
                 </div>
             </form>
-</div>
+        </div>
     </div>
 </body>
 
